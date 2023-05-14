85912dc2d54828da17eb46db602f1e78318848c6
Roman Podhornykh <romanpodgornykh@gmail.com>
2023-05-15 01:17:59 +0300

    150523-2

--- src/main/java/org/example/SearchDevice.java
+++ src/main/java/org/example/SearchDevice.java
@@ -2,9 +2,11 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,7 +20,7 @@ public class SearchDevice {
         System.setProperty("webdriver.chrome.driver", "C:\\IdeaProjects\\chromedriver.exe");
         this.driver = new ChromeDriver(co);
         driver.get("https://www.amazon.com/");
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();   //Browser setup and open homepage
     }
         @Test
         public void checkdevice () {
@@ -29,5 +31,13 @@ public class SearchDevice {
             Assert.assertEquals(actualURL, currentURL);
             driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Google Pixel 6a");
             driver.findElement(By.id("nav-search-submit-button")).click();
+
+            WebElement successfulmsg= driver.findElement(By.xpath("//span[contains(text(),'\"Google Pixel 6a\"')]"));
+            String expectedmsg="Google Pixel 6a";
+            String actualmsg=successfulmsg.getText();
+            Assert.assertTrue(actualmsg.contains(expectedmsg), "doesn't contain expectedmsg");
+            driver.quit();     //Test check if correct results were shown
         }
+
+
     }
