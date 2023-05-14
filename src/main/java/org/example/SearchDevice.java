34906e0edc21af38c9ffe48cf2b5c7ce9ed65984
Roman Podhornykh <romanpodgornykh@gmail.com>
2023-05-15 00:43:24 +0300

    150523

--- src/main/java/org/example/SearchDevice.java
+++ src/main/java/org/example/SearchDevice.java
@@ -12,17 +12,17 @@ public class SearchDevice {
     public WebDriver driver;
 
     @BeforeMethod
-    public static void main(String[] args) throws InterruptedException {
+    public void SetUp() throws InterruptedException {
         ChromeOptions co = new ChromeOptions();
         co.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "C:\\IdeaProjects\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(co);
+        this.driver = new ChromeDriver(co);
         driver.get("https://www.amazon.com/");
         driver.manage().window().maximize();
     }
         @Test
         public void checkdevice () {
-            WebDriver driver = new ChromeDriver();
+
             System.out.println(driver.getTitle() + " =This is my title");
             String currentURL = "https://www.amazon.com/";
             String actualURL = driver.getCurrentUrl();
